fe9816f0e71d0b500db700aaaa2a55a01c4e9a40
Ogaday <w.ogaday@gmail.com>
2015-08-10 20:40:55 +0100

    Add hello world program from wikibooks

---
+++ hello.cc
@@ -0,0 +1,15 @@
+/*
+'Hello World!' program
+ Name: hello.cc
+ Source: https://en.wikibooks.org/wiki/C%2B%2B_Programming/Examples/Hello_world
+ Compile with $ g++ hello.cc -o hello
+ Run with $ hello
+*/
+ 
+#include <iostream>
+ 
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
